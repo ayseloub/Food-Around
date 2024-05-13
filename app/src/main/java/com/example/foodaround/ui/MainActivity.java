c25830fa526087156a9c4aa2c754e41b87777706
Muhammad Mirza <mirzaalam557@gmail.com>
2024-05-14 06:39:36 +0700

    Sesudah Konversi Fragment

--- app/src/main/java/com/example/foodaround/ui/MainActivity.java
+++ app/src/main/java/com/example/foodaround/ui/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.foodaround.ui;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.foodaround.R;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,16 +18,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //navbar
-        ImageButton btnHome = findViewById(R.id.btn_home);
-        btnHome.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, HomepageActivity.class);
-            startActivity(intent);
+        ImageButton btnNotif = findViewById(R.id.btn_notif);
+        btnNotif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, new ReviewFragment());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
         });
 
-        ImageButton btnNotif = findViewById(R.id.btn_notif);
-        btnNotif.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, ReviewActivity.class);
-            startActivity(intent);
+        ImageButton btnHome = findViewById(R.id.btn_home);
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, new HomepageFragment());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
         });
+
     }
 }
+
--- app/src/main/java/com/example/foodaround/ui/ReviewFragment.java
+++ app/src/main/java/com/example/foodaround/ui/ReviewFragment.java
@@ -2,55 +2,53 @@ package com.example.foodaround.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+
 import com.example.foodaround.R;
 import com.example.foodaround.database.ReviewEntity;
 
-public class ReviewActivity extends AppCompatActivity {
+public class ReviewFragment extends Fragment {
 
     private RecyclerView recyclerView;
     private LinearLayout layAddReview;
     private EditText etNama, etAlamat, etNamaresto, etReview;
-    private Button btnClear, btnSubmit;
+    private Button btnClear, btnSubmit, btnHome;
 
     private ReviewViewModel reviewViewModel;
     private ReviewAdapter reviewAdapter;
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_review);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_review, container, false);
 
-        //navbar
-        ImageButton btnHome = findViewById(R.id.btn_home);
-        btnHome.setOnClickListener(v -> {
-            Intent intent = new Intent(ReviewActivity.this, HomepageActivity.class);
-            startActivity(intent);
-        });
-
-        recyclerView = findViewById(R.id.rv_review);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView = rootView.findViewById(R.id.rv_review);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         recyclerView.setHasFixedSize(true);
 
-        layAddReview = findViewById(R.id.layout_add);
-        etNama = findViewById(R.id.et_nama);
-        etAlamat = findViewById(R.id.et_alamat);
-        etNamaresto = findViewById(R.id.et_namaresto);
-        etReview = findViewById(R.id.et_review);
-        btnClear = findViewById(R.id.btn_clear);
-        btnSubmit = findViewById(R.id.btn_submit);
+        layAddReview = rootView.findViewById(R.id.layout_add);
+        etNama = rootView.findViewById(R.id.et_nama);
+        etAlamat = rootView.findViewById(R.id.et_alamat);
+        etNamaresto = rootView.findViewById(R.id.et_namaresto);
+        etReview = rootView.findViewById(R.id.et_review);
+        btnClear = rootView.findViewById(R.id.btn_clear);
+        btnSubmit = rootView.findViewById(R.id.btn_submit);
 
-        reviewViewModel = new ViewModelProvider(this).get(ReviewViewModel.class);
+        reviewViewModel = new ViewModelProvider(requireActivity()).get(ReviewViewModel.class);
 
         btnClear.setOnClickListener(v -> clearData());
 
@@ -59,7 +57,7 @@ public class ReviewActivity extends AppCompatActivity {
                     etAlamat.getText().toString().equals("") ||
                     etNamaresto.getText().toString().equals("") ||
                     etReview.getText().toString().equals("")) {
-                Toast.makeText(this, "Please fill in the entire form", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "Please fill in the entire form", Toast.LENGTH_SHORT).show();
             } else {
                 ReviewEntity review = new ReviewEntity(
                         etNama.getText().toString(),
@@ -75,12 +73,12 @@ public class ReviewActivity extends AppCompatActivity {
         reviewAdapter = new ReviewAdapter();
         recyclerView.setAdapter(reviewAdapter);
 
-        reviewViewModel.getAllReviews().observe(this, reviews -> {
+        reviewViewModel.getAllReviews().observe(getViewLifecycleOwner(), reviews -> {
             reviewAdapter.setReviews(reviews);
             reviewAdapter.setOnItemClickListener(new ReviewAdapter.OnItemClickListener() {
                 @Override
                 public void onItemClick(ReviewEntity review) {
-                    Intent intent = new Intent(ReviewActivity.this, DetailReviewActivity.class);
+                    Intent intent = new Intent(getActivity(), DetailReviewActivity.class);
                     intent.putExtra("cnama", review.getNama());
                     intent.putExtra("calamat", review.getAlamat());
                     intent.putExtra("cnamaresto", review.getNamaresto());
@@ -91,10 +89,12 @@ public class ReviewActivity extends AppCompatActivity {
                 @Override
                 public void onDeleteClick(ReviewEntity review) {
                     reviewViewModel.delete(review);
-                    Toast.makeText(ReviewActivity.this, "Item deleted", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getActivity(), "Item deleted", Toast.LENGTH_SHORT).show();
                 }
             });
         });
+
+        return rootView;
     }
 
     private void clearData() {
@@ -104,4 +104,6 @@ public class ReviewActivity extends AppCompatActivity {
         etReview.setText("");
     }
 }
+
+
 
